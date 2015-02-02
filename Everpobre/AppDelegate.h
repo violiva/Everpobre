e4295c01e55fe287ed5e99c1491f0b4e095ecd41
Vicente Oliva <vicente_Oliva@hotmail.com>
2015-02-02 12:05:26 +0100

    Initial Commit

---
+++ Everpobre/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Everpobre
+//
+//  Created by Vicente Oliva de la Serna on 2/2/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Everpobre/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Everpobre
+//
+//  Created by Vicente Oliva de la Serna on 2/2/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
